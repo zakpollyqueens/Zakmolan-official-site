8d228e3f2f6580e80690844c07dc01e6f7199ff9
Zakmolanitech <zakpollyqueens@gmail.com>
2026-08-09 18:25:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,19 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html")
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
 @app.route("/services")
 def services():
     return render_template("services.html")
